fd86b42d1c4fe972d0536eacd34e1fab74abbc4f
akashin <kashin.andrej@gmail.com>
2019-04-27 12:54:52 +0100

    Add Blood level to the player state

--- src/logic/playerState.ts
+++ src/logic/playerState.ts
@@ -1,11 +1,17 @@
 // Stores player characteristics.
 export class PlayerState {
-  // Stores:
-  // - Current blood level
-  // - Upgrades
-
   // Contains logic about cost of different operations, e.g. shooting.
 
-  constructor() {
+  // Stores:
+  // - Upgrades
+
+  // - Current blood level
+  private _blood: number;
+  blood(): number {
+    return this._blood;
+  }
+
+  constructor(starting_blood) {
+    this._blood = starting_blood;
   }
 }
