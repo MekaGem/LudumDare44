4015ae05c34bb77fb1b2bbe3a3f4eb635836d51f
Osipov Andrew <mekagem@gmail.com>
2019-04-29 18:07:03 +0300

    fix blood

--- src/scenes/mainScene.ts
+++ src/scenes/mainScene.ts
@@ -24,7 +24,7 @@ export class MainScene extends Phaser.Scene {
   // Stores tiles images.
   private tileset: Phaser.Tilemaps.Tileset;
   // Display object derived from the tilemap.
-  private worldLayer: Phaser.Tilemaps.StaticTilemapLayer;
+  private worldLayer: Phaser.Tilemaps.DynamicTilemapLayer;
   private backgroundLayer: Phaser.Tilemaps.StaticTilemapLayer;
 
   private enemiesGroup: Phaser.Physics.Arcade.Group;
@@ -81,7 +81,7 @@ export class MainScene extends Phaser.Scene {
 
     this.backgroundLayer = this.tilemap.createStaticLayer("Background", this.tileset, 0, 0);
 
-    this.worldLayer = this.tilemap.createStaticLayer("World", this.tileset, 0, 0);
+    this.worldLayer = this.tilemap.createDynamicLayer("World", this.tileset, 0, 0);
     this.worldLayer.setScale(1);
     this.worldLayer.setCollisionByProperty({ collides: true });
 
